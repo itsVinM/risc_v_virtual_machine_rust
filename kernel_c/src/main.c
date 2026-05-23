a55ac168192ca548746e0529d593bc8a9bb4576f
itsVinM <vincentiu.mocanu00@gmail.com>
2026-05-23 11:13:38 +0200

    add: main.c for kernel

--- kernel_c/src/main.c
+++ kernel_c/src/main.c
@@ -1,19 +1,46 @@
 #include <stdint.h>
 
+#define MTIME_ADDR      0x200BFF8
+#define MTIMECMP_ADDR   0x2004000
+#define TIMER_INTERVAL  500
+
 volatile uint64_t ticks = 0;
 
-// call assembly trap handler
-void handler_interrupt(void){
-    ticks++; //asm rearm the timer
+static inline uint64_t read_u64(uint64_t addr) {
+    uint64_t val;
+    asm volatile("ld %0, 0(%1)" : "=r"(val) : "r"(addr));
+    return val;
 }
 
-int main(void){
-    while(ticks < 5){
-        // spin waiting for interrupts 
-    }
+static inline void write_u64(uint64_t addr, uint64_t val) {
+    asm volatile("sd %0, 0(%1)" : : "r"(val), "r"(addr));
+}
 
-    // halt
+static inline void csrs(uint64_t csr_num, uint64_t val) {
+    asm volatile("csrs %0, %1" : : "i"(csr_num), "r"(val));
+}
+
+void handler_interrupt(void) {
+    ticks++;
+}
+
+int main(void) {
+    uint64_t trap_addr;
+    uint64_t current_time;
+    uint64_t next_interrupt;
+
+    asm volatile("la %0, _trap" : "=r"(trap_addr));
+    asm volatile("csrw mtvec, %0" : : "r"(trap_addr));
+
+    current_time = read_u64(MTIME_ADDR);
+    next_interrupt = current_time + TIMER_INTERVAL;
+    write_u64(MTIMECMP_ADDR, next_interrupt);
+
+    csrs(0x304, (1 << 7));   // mie.MTIE
+    csrs(0x300, (1 << 3));   // mstatus.MIE
+
+    while (ticks < 5) { }
+
     asm volatile("ebreak");
-
     return 0;
 }
